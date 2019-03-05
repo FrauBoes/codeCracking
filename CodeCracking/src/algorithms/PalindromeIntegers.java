b840f17f12e4ad6e09a4daaa6ddc9e081791dcff
frauboes <juliaboes@posteo.de>
2019-03-05 19:13:14 +0000

    add palindrome for integer

---
+++ CodeCracking/src/algorithms/PalindromeIntegers.java
@@ -0,0 +1,26 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// String with lower and upper case chars and whitespace
+public class PalindromeIntegers {
+	
+	public static void main(String[] args) {
+		System.out.print(isPali(121));
+	}
+	
+	public static boolean isPali(int n) {
+		
+		int rev = 0;
+		int num = n;
+		
+		while (n > 0) {
+			int digit = n % 10;
+			rev = rev * 10 + digit;
+			n /= 10;
+		}
+			
+		return rev == num;
+	}
+}
