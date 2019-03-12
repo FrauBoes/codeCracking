6e3c7ab9675f52b633b0e95ec1f6607c9b28ceef
frauboes <juliaboes@posteo.de>
2019-03-12 16:49:24 +0000

    add string compression

---
+++ CodeCracking/src/stringManipulation/CompressString.java
@@ -0,0 +1,38 @@
+package stringManipulation;
+
+public class CompressString {
+	public static void main(String[] args) {
+		System.out.println(compress("XXY"));
+	}
+	
+	public static String compress(String s) {
+		StringBuilder compressed = new StringBuilder();
+		
+		// Only start compression if compressed string can shorten length
+		// Otherwise, or if s is empty, return s
+		if (s.length() <= 2)
+			return s;
+		
+		else {
+			int count = 1;
+			char current = s.charAt(0);
+			
+			for (int i = 1; i < s.length(); i++) {
+				if (s.charAt(i) == current)
+					count++;
+				
+				else {
+					compressed.append(Integer.toString(count) + current);
+					count = 1;
+					current = s.charAt(i);
+				}
+			}
+			
+			compressed.append(Integer.toString(count) + current);
+		}
+		
+		// If original string is shorter, return it
+		// Otherwise return compressed string
+		return s.length() < compressed.toString().length() ? s : compressed.toString();
+	}
+}
